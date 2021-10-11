f8ba3161208f7f9064db93c335a90e64950fa00d
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2021-10-11 15:38:57 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,7 +8,7 @@ async def on_message(message: discord.Message):
 
     if message.content == "s.join":
         if message.author.voice is None:
-            await message.channel.send("VCチャンネルに接続してください。")
+            await message.channel.send("VCチャンネルに接続してから、もう１度お試しください。")
             return
         await message.author.voice.channel.connect()
         await message.channel.send("接続しました。")
@@ -20,31 +20,31 @@ async def on_message(message: discord.Message):
         await message.guild.voice_client.disconnect()
         await message.channel.send("切断しました。")
     
-    if message.content == "s.play time":
+    if message.content == "s.p time":
         if message.guild.voice_client is None:
-            await message.channel.send("接続していません。")
+            await message.channel.send("接続していません。VCチャンネルに接続してから、もう１度お試しください。")
         message.guild.voice_client.play(discord.FFmpegPCMAudio("time.mp3"))  
         return
           
-    if message.content == "s.play kansei":
+    if message.content == "s.p kansei":
         if message.guild.voice_client is None:
-            await message.channel.send("接続していません。")
+            await message.channel.send("接続していません。VCチャンネルに接続してから、もう１度お試しください。")
         message.guild.voice_client.play(discord.FFmpegPCMAudio("kansei.mp3"))  
         return
 
-    if message.content == "s.play countdown":
+    if message.content == "s.p count":
         if message.guild.voice_client is None:
-            await message.channel.send("接続していません。")
+            await message.channel.send("接続していません。VCチャンネルに接続してから、もう１度お試しください。")
         message.guild.voice_client.play(discord.FFmpegPCMAudio("countdown.mp3"))  
         return
     
-    if message.content == "s.play bunka":
+    if message.content == "s.p bunka":
         if message.guild.voice_client is None:
-            await message.channel.send("接続していません。")
+            await message.channel.send("接続していません。VCチャンネルに接続してから、もう１度お試しください。")
         message.guild.voice_client.play(discord.FFmpegPCMAudio("bunka.mp3"))  
         return
 
     if message.content =="s.help":
-        await message.channel.send("コマンド一覧\n`s.join` コマンドを打った人が居るVCチャンネルに接続\n`s.leave` VCチャンネルから切断\n`s.play countdown` 321beatboxの音声\n`s.play time` timeの音声\n`s.play kansei` 歓声")
-
+        await message.channel.send("コマンド一覧\n`s.join` コマンドを打った人が居るVCチャンネルに接続\n`s.leave` VCチャンネルから切断\n`s.p count` 321beatboxの音声\n`s.p time` timeの音声\n`s.p kansei` 歓声\n`s.p bunka` 文化の人の音声")
+        await message.channel.send("bot開発者：tari3210 #9924\nコマンド追加要望やバグ等ありましたら、いつでもご連絡ください。")
 client.run("ODk2NjUyNzgzMzQ2OTE3Mzk2.YWKO-g.PbWqRCFnvgd0YGAOMAHNqDKNQAU")
