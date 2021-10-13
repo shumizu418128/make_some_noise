00dd329bfbce259b60945083567903e7c612f148
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2021-10-13 19:40:39 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -59,7 +59,14 @@ async def on_message(message: discord.Message):
     if message.content == "s.p esh":
         if message.guild.voice_client is None:
             await message.channel.send("接続していません。VCチャンネルに接続してから、もう一度お試しください。")
-        audio = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("esh.mp3"), volume=0.3)
+        audio = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("esh.mp3"), volume=0.4)
+        message.guild.voice_client.play(audio)
+        return
+
+    if message.content == "s.p msn":
+        if message.guild.voice_client is None:
+            await message.channel.send("接続していません。VCチャンネルに接続してから、もう一度お試しください。")
+        audio = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("msn.mp3"), volume=0.3)
         message.guild.voice_client.play(audio)
         return
 
@@ -84,7 +91,7 @@ async def on_message(message: discord.Message):
             return
 
     if message.content == "s.help":
-        await message.channel.send("コマンド一覧\n`s.join` コマンドを打った人が居るVCチャンネルに接続\n`s.leave` VCチャンネルから切断\n`s.t`タイマーを利用できます。詳細はs.help timeと入力すると確認できます。\n`s.p count` 321beatboxの音声\n`s.p time` timeの音声\n`s.p kbbtime` 歓声無しtimeの音声 音源：KBB\n`s.p kansei` 歓声\n`s.p bunka` 文化の人の音声\n`s.p esh` eshの音声")
+        await message.channel.send("コマンド一覧\n`s.join` コマンドを打った人が居るVCチャンネルに接続\n`s.leave` VCチャンネルから切断\n`s.t` タイマーを利用できます。詳細はs.help timeと入力すると確認できます。\n`s.p count` 321beatboxの音声\n`s.p time` timeの音声\n`s.p kbbtime` 歓声無しtimeの音声 音源：KBB\n`s.p kansei` 歓声\n`s.p bunka` 文化の人の音声\n`s.p esh` eshの音声\n`s.p msn` make some noiseの音声")
         await message.channel.send("bot開発者：tari3210 #9924\nコマンド追加要望やバグ等ありましたら、いつでもご連絡ください。")
 
     if message.content == "s.help time":
