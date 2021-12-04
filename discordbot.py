643a0de4f858af33d5f8f972dc17fa1f11e8d38b
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2021-12-04 09:30:18 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,5 @@
 import discord
 import random
-import numpy as np
 from time import sleep
 client = discord.Client()
 print("successfully started")
@@ -189,23 +188,5 @@ async def on_message(message: discord.Message):
         sleep(1)
         await message.channel.send("TIME!")
         return
-
-    if "s.battle" in message.content:
-        names = [(j) for j in message.content.split()]
-        names.remove("s.battle")
-        num = np.arange(0, len(names))
-        np.random.shuffle(num)
-        count, count2 = 0, 1
-        await message.channel.send("処理に時間がかかります。\n「処理終了」と表示されるまで **何も書き込まず** お待ちください。\n対戦カード：")
-        while count < len(names):
-            await message.channel.send("第" + str(count2) + "試合：" + names[num[count]] + " VS " + names[num[count + 1]])
-            count += 2
-            count2 += 1
-        list = []
-        for i in range(len(names)):
-            print1 = str(names[num[i]])
-            list.append(print1)
-        list = ', '.join(list)
-        await message.channel.send("トーナメント表書き込み順：\n" + list + "\n\n処理終了")
 
 client.run("ODk2NjUyNzgzMzQ2OTE3Mzk2.YWKO-g.PbWqRCFnvgd0YGAOMAHNqDKNQAU")
