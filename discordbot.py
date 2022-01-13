5037b4c6b8799637da44a203274cc94ce39b58cc
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2022-01-13 15:39:50 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -334,7 +334,7 @@ async def on_message(message):
         counter2 = 0
         dt_now = datetime.datetime.now()
         date = str(dt_now.strftime('%m月%d日 %H:%M'))
-        embed = discord.Embed(title="抽選結果", description="%d" % (date), color=0xff9900)
+        embed = discord.Embed(title="抽選結果", description="%s" % (date), color=0xff9900)
         while counter2 + 2 <= len(playerlist):
             embed.add_field(name="Match%s" % (str(counter)), value="%s `1st` vs %s `2nd`" % (playerlist[counter2], playerlist[counter2 + 1]), inline=False)
             counter += 1
