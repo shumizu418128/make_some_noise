7f82216fe0f1d64bad06e15ebb583d69d73b6a3f
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2022-01-26 23:14:50 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -286,7 +286,8 @@ async def on_message(message):
         if len(names) != 3:
             await message.channel.send("Error: 入力方法が間違っています。")
             return
-        await message.channel.send(names[1] + "さん `1st` vs " + names[2] + "さん `2nd`\n\n1分・2ラウンドずつ\n1 minute, 2 rounds each\n\nAre you ready??")
+        names.remove("ss.battle")
+        await message.channel.send(names[0] + "さん `1st` vs " + names[1] + "さん `2nd`\n\n1分・2ラウンドずつ\n1 minute, 2 rounds each\n\nAre you ready??")
         audio = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("battle_start.mp3"), volume=0.5)
         message.guild.voice_client.play(audio)
         await sleep(12)
@@ -312,7 +313,7 @@ async def on_message(message):
             except AttributeError:
                 await message.channel.send("Error: 接続が失われたため、タイマーを停止しました\nlost connection")
                 return
-            embed = discord.Embed(title="残り40秒", description="Round%s" % (str(i + 1)), color=0x00ff00)
+            embed = discord.Embed(title="残り40秒", description="Round%s %s" % (str(i + 1), names[0]), color=0x00ff00)
             await message.channel.send(embed=embed)
             await sleep(10)
             try:
@@ -326,7 +327,7 @@ async def on_message(message):
             except AttributeError:
                 await message.channel.send("Error: 接続が失われたため、タイマーを停止しました\nlost connection")
                 return
-            embed = discord.Embed(title="残り20秒", description="Round%s" % (str(i + 1)), color=0xffff00)
+            embed = discord.Embed(title="残り20秒", description="Round%s %s" % (str(i + 1), names[0]), color=0xffff00)
             await message.channel.send(embed=embed)
             await sleep(10)
             try:
@@ -334,7 +335,7 @@ async def on_message(message):
             except AttributeError:
                 await message.channel.send("Error: 接続が失われたため、タイマーを停止しました\nlost connection")
                 return
-            embed = discord.Embed(title="残り10秒", description="Round%s" % (str(i + 1)), color=0xff0000)
+            embed = discord.Embed(title="残り10秒", description="Round%s %s" % (str(i + 1), names[0]), color=0xff0000)
             await message.channel.send(embed=embed)
             await sleep(9)
             if i < 3:
@@ -344,7 +345,8 @@ async def on_message(message):
                 except AttributeError:
                     await message.channel.send("Error: 接続が失われたため、タイマーを停止しました\nlost connection")
                     return
-                await message.channel.send("--------------------\n\nTIME!\nRound%s SWITCH!\n\n--------------------" % (str(i + 2)))
+                await message.channel.send("--------------------\n\nTIME!\nRound%s %s\nSWITCH!\n\n--------------------" % (str(i + 2), names[1]))
+                names.reverse()
                 await sleep(3)
             elif i == 3:
                 audio = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("time.mp3"), volume=0.2)
@@ -355,7 +357,7 @@ async def on_message(message):
                     return
                 embed = discord.Embed(title="TIME!")
                 await message.channel.send(embed=embed)
-        embed = discord.Embed(title="投票箱", description="`1st:`%s\n`2nd:`%s\n\nぜひ気に入ったBeatboxerさんに1票をあげてみてください。\n※集計は行いません。botの動作はこれにて終了です。" % (names[1], names[2]))
+        embed = discord.Embed(title="投票箱", description="`1st:`%s\n`2nd:`%s\n\nぜひ気に入ったBeatboxerさんに1票をあげてみてください。\n※集計は行いません。botの動作はこれにて終了です。" % (names[1], names[0]))
         message3 = await message.channel.send(embed=embed)
         await message3.add_reaction("1⃣")
         await message3.add_reaction("2⃣")
@@ -460,6 +462,21 @@ async def on_message(message):
         embed.title = "対戦カード"
         await channel0.send(embed=embed)
         await channel0.send("%s\n %s を確認して、マイク設定を行ってからの参加をお願いします。" % (role.mention, bbx_mic.mention))
+        return
+
+    if message.content == "s.stage":
+        stage_channel = client.get_channel(931462636019802123)  # ステージ
+        try:
+            await stage_channel.create_instance(topic="battle stadium")
+        except discord.errors.HTTPException:
+            pass
+        try:
+            await stage_channel.connect(reconnect=True)
+        except discord.errors.ClientException:
+            pass
+        guild = client.get_guild(864475338340171786)  # サーバーID
+        me = guild.get_member(897751398702264360)  # make some noise! - local
+        await me.edit(suppress = False)
         return
 
     if "s." not in message.content:
