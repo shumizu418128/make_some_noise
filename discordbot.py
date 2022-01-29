a238bec90a0cdb2977ab2c7eb95dbe9927ce2ff0
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2022-01-29 12:32:47 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -357,7 +357,7 @@ async def on_message(message):
                     return
                 embed = discord.Embed(title="TIME!")
                 await message.channel.send(embed=embed)
-        embed = discord.Embed(title="投票箱", description="`1st:` %s\n`2nd: `%s\n\nぜひ気に入ったBeatboxerさんに1票をあげてみてください。\n※集計は行いません。botの動作はこれにて終了です。" % (names[1], names[0]))
+        embed = discord.Embed(title="投票箱", description="`1st:` %s\n`2nd:` %s\n\nぜひ気に入ったBeatboxerさんに1票をあげてみてください。\n※集計は行いません。botの動作はこれにて終了です。" % (names[1], names[0]))
         message3 = await message.channel.send(embed=embed)
         await message3.add_reaction("1⃣")
         await message3.add_reaction("2⃣")
