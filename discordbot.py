d70b819ab330033d976f6841eedd1695d01e01d7
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2023-02-05 15:23:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -38,13 +38,13 @@ async def on_voice_state_update(member, before, after):
 async def on_member_join(member):
     channel = client.get_channel(864475338340171791)  # 全体チャット
     events = channel.guild.scheduled_events
+    if bool(events) is False:
+        return
     min = [None, events[0].start_time]
     for event in events:
         if event.start_time < min[1]:
             min = [event, event.start_time]
     event = min[0]
-    if event is None:
-        return
     await sleep(1)
     await channel.send(event.url)
 
