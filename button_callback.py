112bdba53dd042daf44c91faeb6a309ce615cfa4
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2023-12-26 12:06:14 +0900

    最終確認noticeも同様に削除

--- button_callback.py
+++ button_callback.py
@@ -290,8 +290,8 @@ async def button_cancel(interaction: Interaction):
 
     # 10秒で処理中止
     except TimeoutError:
-        await notice.clear_reactions()
-        await notice.reply("Error: Timeout\nもう1度お試しください")
+        await notice.delete()
+        await interaction.channel.send("Error: Timeout\nもう1度お試しください")
         return
 
     await notice.clear_reactions()
